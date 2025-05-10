78e88210f5c9f9f24779afd701d1d93a7397222f
Megha Varshini <Varshinim173@gmail.com>
2025-05-10 21:59:09 +0530

    Update Myworksection.tsx

--- src/components/Myworksection.tsx
+++ src/components/Myworksection.tsx
@@ -51,7 +51,7 @@ const Myworksection = () => {
                 href="https://ai-resume-landing-liard.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-[#D9D9D9] text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 hidden md:block text-center mt-1"
+                className="bg-[#D9D9D9] text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 hidden md:block text-center mt-2"
               >
                 <button>
                   Live Demo
@@ -73,7 +73,7 @@ const Myworksection = () => {
               href="https://ai-resume-landing-liard.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-[#D9D9D9] md:hidden block text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 text-center mt-1"
+              className="bg-[#D9D9D9] md:hidden block text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 text-center mt-2"
             >
               <button>
                 Live Demo
@@ -113,7 +113,7 @@ const Myworksection = () => {
                 href="https://nocode-landing-page.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-[#D9D9D9] text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 hidden md:block text-center mt-1"
+                className="bg-[#D9D9D9] text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 hidden md:block text-center mt-2"
               >
                 <button>
                   Live Demo
@@ -135,7 +135,7 @@ const Myworksection = () => {
               href="https://nocode-landing-page.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-[#D9D9D9] md:hidden block text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 text-center mt-1"
+              className="bg-[#D9D9D9] md:hidden block text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 text-center mt-2"
             >
               <button>
                 Live Demo
