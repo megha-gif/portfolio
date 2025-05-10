586d2ca9c21dd68dd5ee8749f11bd288e12c432a
Megha Varshini <Varshinim173@gmail.com>
2025-05-10 21:43:11 +0530

    hi

--- src/components/Myworksection.tsx
+++ src/components/Myworksection.tsx
@@ -51,7 +51,7 @@ const Myworksection = () => {
                 href="https://ai-resume-landing-liard.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-[#D9D9D9] text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 hidden md:block
+                className="bg-[#D9D9D9] text-[17px] rounded-[4px] mt-8 pt-[3px] w-[110px] h-[35px] font-poppins text-[#000000] hover:scale-104 hidden md:block"
               >
                 <button>
                   Live Demo
